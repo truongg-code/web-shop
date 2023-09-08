88fe9f967cbcff187e0347727722379ae2886800
Truong09-karry <truonghk28@gmail.com>
2023-09-08 22:43:23 +0700

    aa
    aaa

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Welcome to my page</h1>
+      <h1>Welcome to truong</h1>
     </main>
   );
 }
